e2799b55e3b9dc5f1e2c94223c9ad5dcb52c58e3
hrddd <nuhhunnuhhun@gmail.com>
2021-04-06 02:36:18 +0900

    add scaleLink

--- src/components/organisms/DangoEditor.tsx
+++ src/components/organisms/DangoEditor.tsx
@@ -1,21 +1,23 @@
+// TODO: テスト
+// height操作でstateが変更される
+// width操作でstateが変更される
+// scaleLink操作でstateが変更される
+// scaleLink操作でheight,widthの更新方法が変更される
+
 import Dango from "../molecules/Dango";
 import InputRange from '../atoms/InputRange';
 import { ChangeEvent, useState } from "react";
+import InputToggle from '../atoms/InputToggle';
+
+const initialState = {
+  width: 72,
+  height: 52,
+  fill: '#aaC8B3',
+  scaleLink: true,
+}
 
 const DangoEditor = () => {
-  const [state, setState] = useState({
-    width: 72,
-    height: 52,
-    fill: '#aaC8B3',
-  })
-  const onChangeHandlerFactory = (key: string) => {
-    return (e: ChangeEvent<HTMLInputElement>) => {
-      setState({
-        ...state,
-        [key]: Number(e.currentTarget.value)
-      })
-    }
-  }
+  const [state, setState] = useState(initialState)
 
   const widthRangeProps = {
     id: 'width',
@@ -24,22 +26,49 @@ const DangoEditor = () => {
     min: 72,
     max: 1440,
     value: state.width,
-    onChange: onChangeHandlerFactory('width')
+    onChange: (e: ChangeEvent<HTMLInputElement>) => {
+      const width = Number(e.currentTarget.value);
+      setState({
+        ...state,
+        width,
+        height: state.scaleLink ? width * initialState.height / initialState.width : state.height
+      })
+    }
   }
   const heightRangeProps = {
     id: 'height',
     label: 'height',
     name: 'height',
-    min: 72,
+    min: 52,
     max: 1028,
     value: state.height,
-    onChange: onChangeHandlerFactory('height')
+    onChange: (e: ChangeEvent<HTMLInputElement>) => {
+      const height = Number(e.currentTarget.value);
+      setState({
+        ...state,
+        width: state.scaleLink ? height * initialState.width / initialState.height : state.width,
+        height
+      })
+    }
+  }
+  const scaleLinkToggleProps = {
+    id: 'scaleLink',
+    label: 'scaleLink',
+    name: 'scaleLink',
+    checked: state.scaleLink,
+    onChange: (e: ChangeEvent<HTMLInputElement>) => {
+      setState({
+        ...state,
+        scaleLink: e.currentTarget.checked
+      })
+    }
   }
   return (
     <>
       <Dango {...state} />
       <InputRange {...widthRangeProps} />
       <InputRange {...heightRangeProps} />
+      <InputToggle {...scaleLinkToggleProps} />
     </>)
 }
 
