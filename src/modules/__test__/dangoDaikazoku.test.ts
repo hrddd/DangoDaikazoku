563485b929ed371bd39328c33403889a6cf97df0
hrddd <nuhhunnuhhun@gmail.com>
2021-04-11 00:13:28 +0900

    add actions test

--- src/modules/__test__/dangoDaikazoku.test.ts
+++ src/modules/__test__/dangoDaikazoku.test.ts
@@ -1,11 +1,64 @@
-import { selectDangoAction, reducer } from '../dangoDaikazoku';
+import { selectDangoAction, reducer, deselectDangoAction, updateDangoAction, addDangoAction, removeDangoAction } from '../dangoDaikazoku';
 
+const dummyDango = (id: string) => ({
+  id,
+  width: 72,
+  height: 52,
+  fill: '#aaC8B3',
+  stroke: '#5D3F35',
+  strokeWidth: 8
+})
 
-describe('dangoDaikazoku reducer', () => {
+describe('Actions', () => {
   it('selectDangoAction: 選択しただんごのidが保持できる', () => {
-    const id = 'dummy_id';
+    const id = 'id';
     const action = selectDangoAction(id);
     const result = reducer({ dangos: [], selectedId: null }, action);
-    expect(result).toEqual({ dangos: [], selectedId: id })
+    expect(result.selectedId).toBe(id)
+  })
+  it('deselectDangoAction: 選択しただんごのidを解除できる', () => {
+    const action = deselectDangoAction();
+    const result = reducer({ dangos: [], selectedId: 'some_id' }, action);
+    expect(result.selectedId).toBe(null)
+  })
+  it('updateDangoAction: 各だんごのパラメータをアップデートできる', () => {
+    const action = updateDangoAction({
+      ...dummyDango('id2'), width: 144
+    });
+    const result = reducer({
+      dangos: [
+        dummyDango('id'),
+        dummyDango('id2'),
+        dummyDango('id3')
+      ], selectedId: null
+    }, action);
+    expect(result.dangos).toEqual([
+      dummyDango('id'),
+      { ...dummyDango('id2'), width: 144 },
+      dummyDango('id3')
+    ])
+  })
+  it('addDangoAction: だんごを追加できる', () => {
+    const action = addDangoAction();
+    const result = reducer({
+      dangos: [
+        dummyDango('id')
+      ], selectedId: null
+    }, action);
+    expect(result.dangos.length).toBe(2)
+  })
+  it('removeDangoAction: だんごを削除できる', () => {
+    const action = removeDangoAction('id2');
+    const result = reducer({
+      dangos: [
+        dummyDango('id'),
+        dummyDango('id2'),
+        dummyDango('id3')
+      ], selectedId: null
+    }, action);
+    expect(result.dangos).toEqual([
+      dummyDango('id'),
+      dummyDango('id3')
+    ])
   })
 })
