403cf7644129b8cfcca9f2f943cc223a2e400713
hrddd <nuhhunnuhhun@gmail.com>
2021-04-19 03:06:54 +0900

    edit viewer presentational

--- src/components/organisms/DangoDaikazoku/Viewer.tsx
+++ src/components/organisms/DangoDaikazoku/Viewer.tsx
@@ -1,35 +1,20 @@
-// TODO: テスト
-// height操作でstateが変更される
-// width操作でstateが変更される
-
-import { useContext } from "react";
 import SelectableDango from "../../molecules/SelectableDango/SelectableDango";
-import { DangoDaikazokuContext, DangoDaikazokuUpdateContext } from '../../../contexts/dangoDaikazokuContext';
-import { deselectDangoAction, selectDangoAction, addDangoAction } from '../../../modules/dangoDaikazoku';
 import styles from './Viewer.module.css'
+import { Dango as DangoType } from '../../../types/Dango';
 
-const DangoDaikazokuViewer = () => {
-  // TODO: customhookに退避
-  const dangoDaikazoku = useContext(DangoDaikazokuContext)
-  const dispatch = useContext(DangoDaikazokuUpdateContext)
-
-  const clickHandler = (e: React.MouseEvent<HTMLDivElement>) => {
-    const clickedId = e.currentTarget.dataset['id'] as string
-    if (dangoDaikazoku && dangoDaikazoku.selectedId !== clickedId) {
-      dispatch(selectDangoAction(clickedId))
-    } else {
-      dispatch(deselectDangoAction())
-    }
-  }
-
+const DangoDaikazokuViewer = ({ dangos, clickHandler }: {
+  dangos: (DangoType & {
+    isSelected: boolean
+  })[],
+  clickHandler: (e: React.MouseEvent<HTMLDivElement>) => void
+}) => {
   return (
     <div className={styles.root}>
-      {dangoDaikazoku?.dangos.map((dangoProps) => {
-        const isSelected = dangoDaikazoku.selectedId === dangoProps.id;
-        const size = Math.floor(dangoProps.width / 72);
+      {dangos.map((props) => {
+        const size = Math.floor(props.width / 72);
         return (
-          <div className={styles[`_item--${size}`]} key={dangoProps.id} >
-            <SelectableDango {...dangoProps} isSelected={isSelected} onClick={clickHandler} />
+          <div className={styles[`_item--${size}`]} key={props.id} >
+            <SelectableDango {...props} isSelected={props.isSelected} onClick={clickHandler} />
           </div>
         )
       })}
--- src/components/organisms/DangoDaikazoku/index.tsx
+++ src/components/organisms/DangoDaikazoku/index.tsx
@@ -1,12 +1,37 @@
 import DangoDaikazokuViewer from './Viewer';
 import DangoEditor from './Editor';
+import { useContext, useMemo } from 'react';
+import { DangoDaikazokuContext, DangoDaikazokuUpdateContext } from '../../../contexts/dangoDaikazokuContext';
+import { selectDangoAction, deselectDangoAction } from '../../../modules/dangoDaikazoku';
 
 
 const DangoDaikazoku = () => {
+  // TODO: customhookに退避
+  const dangoDaikazoku = useContext(DangoDaikazokuContext)
+  const dispatch = useContext(DangoDaikazokuUpdateContext)
+
+  const clickHandler = (e: React.MouseEvent<HTMLDivElement>) => {
+    const clickedId = e.currentTarget.dataset['id'] as string
+    if (dangoDaikazoku && dangoDaikazoku.selectedId !== clickedId) {
+      dispatch(selectDangoAction(clickedId))
+    } else {
+      dispatch(deselectDangoAction())
+    }
+  }
+
+  const dangos = useMemo(() => {
+    return dangoDaikazoku?.dangos.map((dango) => ({
+      ...dango,
+      isSelected: dangoDaikazoku.selectedId === dango.id
+    }))
+  }, [dangoDaikazoku])
   return (
     <>
       <DangoEditor />
-      <DangoDaikazokuViewer />
+      { dangos
+        ? (
+          <DangoDaikazokuViewer dangos={dangos} clickHandler={clickHandler} />
+        ) : ''}
     </>)
 }
 
