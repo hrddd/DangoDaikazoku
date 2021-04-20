ff2c8442ea613cc7f2284555b499c5b6302ca1d0
hrddd <nuhhunnuhhun@gmail.com>
2021-04-21 01:48:11 +0900

    edit editor presentational

--- src/components/organisms/DangoDaikazoku/Editor.tsx
+++ src/components/organisms/DangoDaikazoku/Editor.tsx
@@ -4,209 +4,37 @@
 
 import Dango from "../../atoms/Dango";
 import InputRange from '../../atoms/InputRange';
-import { ChangeEvent, useContext, useEffect, useState } from "react";
 import InputColor from '../../atoms/InputColor';
-import { DangoDaikazokuContext, DangoDaikazokuUpdateContext } from "../../../contexts/dangoDaikazokuContext";
 import Button from "../../atoms/Button";
-import { updateDangoAction, addDangoAction, removeDangoAction } from '../../../modules/dangoDaikazoku';
 import InputToggle from '../../atoms/InputToggle';
 import { Dango as DangoType } from '../../../types/Dango';
 
-type DangoChangeableParameters = Omit<DangoType, 'id'>
-const dafaultState: DangoChangeableParameters = {
-  width: 72,
-  height: 52,
-  fill: '#aaC8B3',
-  stroke: '#5D3F35',
-  strokeWidth: 8
-}
-
-const DangoEditor = () => {
-  // TODO: customhookに退避
-  const dangoDaikazoku = useContext(DangoDaikazokuContext)
-  const dispatch = useContext(DangoDaikazokuUpdateContext)
-  const selectedDango = dangoDaikazoku?.dangos.find(({ id }) => id === dangoDaikazoku.selectedId)
-
-  // TODO: editor用のcontextを形成
-  // {
-  //   dango: {},
-  //   options: {
-  //     randomize: true
-  //   }
-  // }
-  const [state, setState] = useState(selectedDango);
-  const [formState, setFormState] = useState({
-    randomize: false
-  });
-
-  useEffect(() => {
-    setState(selectedDango)
-  }, [selectedDango])
-
-  const randomizeProps = {
-    id: 'randomize',
-    labelText: 'パラメータを適当に',
-    name: 'randomize',
-    checked: formState.randomize,
-    onChange: (e: ChangeEvent<HTMLInputElement>) => {
-      setFormState({
-        ...formState,
-        randomize: e.currentTarget.checked,
-      })
-    }
+const DangoEditor = ({ dango, inputProps }: {
+  dango: Omit<DangoType, 'id'>,
+  inputProps: {
+    randomize: any,
+    widthRange: any,
+    heightRange: any,
+    fill: any,
+    stroke: any,
+    strokeWidth: any,
+    apply: any,
+    copy: any,
+    remove: any,
   }
-  const widthRangeProps = {
-    id: 'width',
-    labelText: 'width',
-    name: 'width',
-    min: 72,
-    max: 720,
-    value: state?.width,
-    disabled: formState.randomize,
-    onChange: (e: ChangeEvent<HTMLInputElement>) => {
-      const width = Number(e.currentTarget.value);
-      if (!state) return void (0);
-      setState({
-        ...state,
-        width,
-        height: Math.ceil(width * dafaultState.height / dafaultState.width)
-      })
-    }
-  }
-  const heightRangeProps = {
-    id: 'height',
-    labelText: 'height',
-    name: 'height',
-    min: 52,
-    max: 520,
-    value: state?.height,
-    disabled: formState.randomize,
-    onChange: (e: ChangeEvent<HTMLInputElement>) => {
-      const height = Number(e.currentTarget.value);
-      if (!state) return void (0);
-      setState({
-        ...state,
-        width: Math.ceil(height * dafaultState.width / dafaultState.height),
-        height
-      })
-    }
-  }
-  const fillProps = {
-    id: 'fill',
-    labelText: 'fill',
-    name: 'fill',
-    value: state?.fill,
-    disabled: formState.randomize,
-    onChange: (e: ChangeEvent<HTMLInputElement>) => {
-      if (!state) return void (0);
-      setState({
-        ...state,
-        fill: e.currentTarget.value,
-      })
-    }
-  }
-  const strokeProps = {
-    id: 'stroke',
-    labelText: 'stroke',
-    name: 'stroke',
-    value: state?.stroke,
-    disabled: formState.randomize,
-    onChange: (e: ChangeEvent<HTMLInputElement>) => {
-      if (!state) return void (0);
-      setState({
-        ...state,
-        stroke: e.currentTarget.value,
-      })
-    }
-  }
-  const strokeWidthProps = {
-    id: 'strokeWidth',
-    labelText: 'strokeWidth',
-    name: 'strokeWidth',
-    min: 1,
-    max: 16,
-    value: state?.strokeWidth,
-    onChange: (e: ChangeEvent<HTMLInputElement>) => {
-      if (!state) return void (0);
-      setState({
-        ...state,
-        strokeWidth: Number(e.currentTarget.value),
-      })
-    }
-  }
-  const randomHexColorFactory = () => {
-    const hex = (number: number) => number.toString(16)
-    let color = '#'
-    for (let i = 0; i < 6; i++) {
-      color += hex(Math.trunc(15.9 * Math.random()))
-    }
-    return color
-  }
-  const randomizeDangoParamFactory = (): DangoChangeableParameters => {
-    const width = 72 + Math.ceil(360 * Math.random());
-    return {
-      width,
-      height: Math.ceil(width * dafaultState.height / dafaultState.width),
-      fill: randomHexColorFactory(),
-      stroke: randomHexColorFactory(),
-      strokeWidth: Math.ceil(8 * Math.random())
-    }
-  }
-
-  const applyProps = {
-    labelText: '変更を反映する',
-    onClick: (e: React.MouseEvent<HTMLButtonElement>) => {
-      if (!state) return void (0);
-      dispatch(updateDangoAction(formState.randomize ? {
-        ...state,
-        ...randomizeDangoParamFactory()
-      } : state))
-    }
-  }
-
-  const copyProps = {
-    labelText: 'だんごをコピーする',
-    onClick: (e: React.MouseEvent<HTMLButtonElement>) => {
-      if (!state) return void (0);
-      const { id, ...copiedParams } = state;
-      dispatch(addDangoAction(formState.randomize ? randomizeDangoParamFactory() : copiedParams))
-    }
-  }
-
-  const removeProps = {
-    labelText: 'だんごを削除する',
-    onClick: (e: React.MouseEvent<HTMLButtonElement>) => {
-      if (!state) return void (0);
-      const { id } = state;
-      dispatch(removeDangoAction(id))
-    }
-  }
-
-  const addProps = {
-    labelText: 'だんごを追加する',
-    onClick: (e: React.MouseEvent<HTMLButtonElement>) => {
-      dispatch(addDangoAction())
-    }
-  }
-
+}) => {
   return (
     <>
-      {
-        state ? (
-          <>
-            <Dango {...state} />
-            <InputToggle {...randomizeProps} />
-            <InputRange {...widthRangeProps} />
-            <InputRange {...heightRangeProps} />
-            <InputColor {...fillProps} />
-            <InputColor {...strokeProps} />
-            <InputRange {...strokeWidthProps} />
-            <Button {...applyProps} />
-            <Button {...copyProps} />
-            <Button {...removeProps} />
-          </>
-        ) : <Button {...addProps} />
-      }
+      <Dango {...dango} />
+      <InputToggle {...inputProps.randomize} />
+      <InputRange {...inputProps.widthRange} />
+      <InputRange {...inputProps.heightRange} />
+      <InputColor {...inputProps.fill} />
+      <InputColor {...inputProps.stroke} />
+      <InputRange {...inputProps.strokeWidth} />
+      <Button {...inputProps.apply} />
+      <Button {...inputProps.copy} />
+      <Button {...inputProps.remove} />
     </>
   )
 }
--- src/components/organisms/DangoDaikazoku/index.tsx
+++ src/components/organisms/DangoDaikazoku/index.tsx
@@ -1,15 +1,232 @@
 import DangoDaikazokuViewer from './Viewer';
 import DangoEditor from './Editor';
-import { useContext, useMemo } from 'react';
+import React, { ChangeEvent, useCallback, useContext, useEffect, useMemo, useState } from 'react';
 import { DangoDaikazokuContext, DangoDaikazokuUpdateContext } from '../../../contexts/dangoDaikazokuContext';
-import { selectDangoAction, deselectDangoAction } from '../../../modules/dangoDaikazoku';
+import { selectDangoAction, deselectDangoAction, addDangoAction, removeDangoAction, updateDangoAction } from '../../../modules/dangoDaikazoku';
+import { Dango as DangoType } from '../../../types/Dango';
+import Button from '../../atoms/Button';
 
+type DangoChangeableParameters = Omit<DangoType, 'id'>
+const dafaultState: DangoChangeableParameters = {
+  width: 72,
+  height: 52,
+  fill: '#aaC8B3',
+  stroke: '#5D3F35',
+  strokeWidth: 8
+}
 
 const DangoDaikazoku = () => {
   // TODO: customhookに退避
   const dangoDaikazoku = useContext(DangoDaikazokuContext)
   const dispatch = useContext(DangoDaikazokuUpdateContext)
 
+  // TODO: editor用のcontextを形成
+  // {
+  //   dango: {},
+  //   inputs: {
+  //     randomize: true
+  //   }
+  // }
+  const [editorState, setEditorState] = useState({
+    dango: undefined as undefined | DangoType,
+    inputs: {
+      width: 72,
+      height: 52,
+      fill: '#aaC8B3',
+      stroke: '#5D3F35',
+      strokeWidth: 8,
+      randomize: true
+    }
+  });
+  const selectEditingDangoProps = useCallback(() => ({
+    editingDango: {
+      width: editorState.inputs.width,
+      height: editorState.inputs.height,
+      fill: editorState.inputs.fill,
+      stroke: editorState.inputs.stroke,
+      strokeWidth: editorState.inputs.strokeWidth,
+    }
+  }), [editorState.inputs])
+
+  useEffect(() => {
+    console.log(editorState)
+    setEditorState({
+      ...editorState,
+      dango: dangoDaikazoku?.dangos.find(({ id }) => id === dangoDaikazoku.selectedId),
+    })
+  }, [dangoDaikazoku?.selectedId])
+
+  // Editor
+  const randomizeProps = {
+    id: 'randomize',
+    labelText: 'パラメータを適当に',
+    name: 'randomize',
+    checked: editorState.inputs.randomize,
+    onChange: (e: ChangeEvent<HTMLInputElement>) => {
+      setEditorState({
+        ...editorState,
+        inputs: {
+          ...editorState.inputs,
+          randomize: e.currentTarget.checked,
+        }
+      })
+    }
+  }
+  const widthRangeProps = {
+    id: 'width',
+    labelText: 'width',
+    name: 'width',
+    min: 72,
+    max: 720,
+    value: editorState.inputs.width,
+    disabled: editorState.inputs.randomize,
+    onChange: (e: ChangeEvent<HTMLInputElement>) => {
+      const width = Number(e.currentTarget.value);
+      setEditorState({
+        ...editorState,
+        inputs: {
+          ...editorState.inputs,
+          width,
+          height: Math.ceil(width * dafaultState.height / dafaultState.width)
+        }
+      })
+    }
+  }
+  const heightRangeProps = {
+    id: 'height',
+    labelText: 'height',
+    name: 'height',
+    min: 52,
+    max: 520,
+    value: editorState.inputs.height,
+    disabled: editorState.inputs.randomize,
+    onChange: (e: ChangeEvent<HTMLInputElement>) => {
+      const height = Number(e.currentTarget.value);
+      setEditorState({
+        ...editorState,
+        inputs: {
+          ...editorState.inputs,
+          width: Math.ceil(height * dafaultState.width / dafaultState.height),
+          height
+        }
+      })
+    }
+  }
+  const fillProps = {
+    id: 'fill',
+    labelText: 'fill',
+    name: 'fill',
+    value: editorState.inputs.fill,
+    disabled: editorState.inputs.randomize,
+    onChange: (e: ChangeEvent<HTMLInputElement>) => {
+      setEditorState({
+        ...editorState,
+        inputs: {
+          ...editorState.inputs,
+          fill: e.currentTarget.value,
+        }
+      })
+    }
+  }
+  const strokeProps = {
+    id: 'stroke',
+    labelText: 'stroke',
+    name: 'stroke',
+    value: editorState.inputs.stroke,
+    disabled: editorState.inputs.randomize,
+    onChange: (e: ChangeEvent<HTMLInputElement>) => {
+      setEditorState({
+        ...editorState,
+        inputs: {
+          ...editorState.inputs,
+          stroke: e.currentTarget.value,
+        }
+      })
+    }
+  }
+  const strokeWidthProps = {
+    id: 'strokeWidth',
+    labelText: 'strokeWidth',
+    name: 'strokeWidth',
+    min: 1,
+    max: 16,
+    value: editorState.inputs.strokeWidth,
+    disabled: editorState.inputs.randomize,
+    onChange: (e: ChangeEvent<HTMLInputElement>) => {
+      setEditorState({
+        ...editorState,
+        inputs: {
+          ...editorState.inputs,
+          strokeWidth: Number(e.currentTarget.value),
+        }
+      })
+    }
+  }
+  const randomHexColorFactory = () => {
+    const hex = (number: number) => number.toString(16)
+    let color = '#'
+    for (let i = 0; i < 6; i++) {
+      color += hex(Math.trunc(15.9 * Math.random()))
+    }
+    return color
+  }
+  const randomizeDangoParamFactory = (): DangoChangeableParameters => {
+    const width = 72 + Math.ceil(360 * Math.random());
+    return {
+      width,
+      height: Math.ceil(width * dafaultState.height / dafaultState.width),
+      fill: randomHexColorFactory(),
+      stroke: randomHexColorFactory(),
+      strokeWidth: Math.ceil(8 * Math.random())
+    }
+  }
+
+  const applyProps = {
+    labelText: '変更を反映する',
+    onClick: (e: React.MouseEvent<HTMLButtonElement>) => {
+      if (!editorState.dango) return void (0);
+      dispatch(updateDangoAction(editorState.inputs.randomize ? {
+        ...editorState.dango,
+        ...randomizeDangoParamFactory()
+      } : {
+          ...editorState.dango,
+          ...editorState.inputs
+        }))
+    }
+  }
+
+  const copyProps = {
+    labelText: 'だんごをコピーする',
+    onClick: (e: React.MouseEvent<HTMLButtonElement>) => {
+      if (!editorState.dango) return void (0);
+      const { id, ...copiedParams } = editorState.dango;
+      dispatch(addDangoAction(editorState.inputs.randomize ? randomizeDangoParamFactory() : copiedParams))
+    }
+  }
+
+  const removeProps = {
+    labelText: 'だんごを削除する',
+    onClick: (e: React.MouseEvent<HTMLButtonElement>) => {
+      if (!editorState.dango) return void (0);
+      const { id } = editorState.dango;
+      dispatch(removeDangoAction(id))
+    }
+  }
+
+  const addProps = {
+    labelText: 'だんごを追加する',
+    onClick: (e: React.MouseEvent<HTMLButtonElement>) => {
+      dispatch(addDangoAction())
+    }
+  }
+
+  // Viewer
+  const dangos = useMemo(() => {
+    return dangoDaikazoku?.dangos.map((dango) => ({
+      ...dango,
+      isSelected: dangoDaikazoku.selectedId === dango.id
+    }))
+  }, [dangoDaikazoku])
   const clickHandler = (e: React.MouseEvent<HTMLDivElement>) => {
     const clickedId = e.currentTarget.dataset['id'] as string
     if (dangoDaikazoku && dangoDaikazoku.selectedId !== clickedId) {
@@ -19,19 +236,26 @@ const DangoDaikazoku = () => {
     }
   }
 
-  const dangos = useMemo(() => {
-    return dangoDaikazoku?.dangos.map((dango) => ({
-      ...dango,
-      isSelected: dangoDaikazoku.selectedId === dango.id
-    }))
-  }, [dangoDaikazoku])
+  const inputProps = {
+    randomize: randomizeProps,
+    widthRange: widthRangeProps,
+    heightRange: heightRangeProps,
+    fill: fillProps,
+    stroke: strokeProps,
+    strokeWidth: strokeWidthProps,
+    apply: applyProps,
+    copy: copyProps,
+    remove: removeProps,
+  }
   return (
     <>
-      <DangoEditor />
-      { dangos
-        ? (
-          <DangoDaikazokuViewer dangos={dangos} clickHandler={clickHandler} />
-        ) : ''}
+      { editorState.dango ? (
+        <DangoEditor
+          dango={selectEditingDangoProps().editingDango} inputProps={inputProps} />
+      ) : (<Button {...addProps} />)}
+      { dangos ? (
+        <DangoDaikazokuViewer dangos={dangos} clickHandler={clickHandler} />
+      ) : ''}
     </>)
 }
 
